075b15deac58c09241fbc666be7718f6b20c771f
hglass <gjdbfl45@naver.com>
2026-05-14 14:21:47 +0900

    애니메이션 수정

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ export default function App() {
             variants={fadeUp}
             initial="hidden"
             whileInView="visible"
-            viewport={{ once: true, amount: 0.2 }}
+            viewport={{ once: true, amount: 0.5 }}
             transition={{ duration: 0.8, ease: "easeOut" }}
           >
             {Screen}
